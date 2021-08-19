e689db9d2cfdad282d0f952a8c72e31cd34301fb
Fahim Ferdous <87265851+UnrequitedHatred@users.noreply.github.com>
2021-08-19 17:45:05 +0600

    Create url_fns.php

---
+++ Code/url_fns.php
@@ -0,0 +1,31 @@
+function add_bm($new_url) {
+
+ echo "Attempting to add ".htmlspecialchars($new_url)."<br />";
+ $valid_user = $_SESSION['valid_user'];
+ $conn = db_connect();
+ $result = $conn->query("select * from bookmark
+where username='$valid_user'
+and bm_URL='".$new_url."'");
+ if ($result && ($result->num_rows>0)) {
+ throw new Exception('Bookmark already exists.');
+ }
+ if (!$conn->query("insert into bookmark values
+ ('".$valid_user."', '".$new_url."')")) {
+ throw new Exception('Bookmark could not be inserted.');
+ }
+ return true;
+} 
+function get_user_urls($username) {
+ $conn = db_connect();
+ $result = $conn->query("select bm_URL
+from bookmark
+where username = '".$username."'");
+ if (!$result) {
+ return false;
+ }
+ $url_array = array();
+ for ($count = 1; $row = $result->fetch_row(); ++$count) {
+ $url_array[$count] = $row[0];
+ }
+ return $url_array;
+}
