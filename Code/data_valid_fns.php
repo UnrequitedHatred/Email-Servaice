ce7e3e426218b2eecd68b711eaa6840e493c4ab6
Fahim Ferdous <87265851+UnrequitedHatred@users.noreply.github.com>
2021-08-19 17:21:58 +0600

    Update data_valid_fns.php

--- Code/data_valid_fns.php
+++ Code/data_valid_fns.php
@@ -6,3 +6,11 @@ return false;
  }
  return true;
 }
+function valid_email($address) {
+ if (preg_match('/^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/', $address))
+{
+ return true;
+ } else {
+ return false;
+ }
+}
