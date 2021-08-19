a6b8e7c897ca2c60a15670aeb7381a6c001e527b
Fahim Ferdous <87265851+UnrequitedHatred@users.noreply.github.com>
2021-08-19 17:39:51 +0600

    Update user_auth_fns.php

--- Code/user_auth_fns.php
+++ Code/user_auth_fns.php
@@ -84,3 +84,24 @@ fseek($fp, 0); // if at end, go to start
  $word = trim($word); 
   return $word;
 }
+  function notify_password($username, $password) {
+ $conn = db_connect();
+ $result = $conn->query("select email from user
+where username='".$username."'");
+ if (!$result) {
+throw new Exception('Could not find email address.');
+ } else if ($result->num_rows == 0) {
+throw new Exception('Could not find email address.');
+ } else {
+$row = $result->fetch_object();
+$email = $row->email;
+$from = "From: support@phpbookmark \r\n";
+$mesg = "Your PHPBookmark password has been changed to ".$password."\r\n".
+"Please change it next time you log in.\r\n";
+if (mail($email, 'PHPBookmark login information', $mesg, $from)) {
+return true;
+} else {
+throw new Exception('Could not send email.');
+}
+ }
+}
